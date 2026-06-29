0462c5f771b5d2f2a5da03cec7a6316249e58db6
Alex <alexdjxyz@gmail.com>
2026-06-29 10:53:30 +0800

    Refine LED matrix command icons

--- Software/uno-q-echoglow/sketch/sketch.ino
+++ Software/uno-q-echoglow/sketch/sketch.ino
@@ -7,60 +7,56 @@
 
 Arduino_LED_Matrix matrix;
 
-const uint32_t IdleFrame[4] = {
+// Default idle display: heart.
+const uint32_t HeartFrame[4] = {
   0x00030c79,
   0xe7fe3fc1,
   0xf80f0060,
   0x00000000,
 };
 
-const uint32_t WarmFrame[4] = {
+// Warmer-light: sun icon.
+const uint32_t SunFrame[4] = {
   0x0602641f,
   0x83fc3fc1,
   0xf8264060,
   0x00000000,
 };
 
-const uint32_t CoolFrame[4] = {
+// Cooler-light: snowflake icon.
+const uint32_t SnowflakeFrame[4] = {
   0x06026416,
-  0x8ffffff1,
+  0x80f00f01,
   0x68264060,
   0x00000000,
 };
 
-const uint32_t BrightFrame[4] = {
-  0xffffffff,
-  0xffffffff,
-  0xffffffff,
+// Brighter: plus icon.
+const uint32_t PlusFrame[4] = {
+  0x00006006,
+  0x01f81f80,
+  0x60060000,
   0x00000000,
 };
 
-const uint32_t DimFrame[4] = {
+// Dimmer: minus icon.
+const uint32_t MinusFrame[4] = {
   0x00000000,
-  0x00600600,
+  0x01f81f80,
   0x00000000,
   0x00000000,
 };
 
-void showFrame(const uint32_t frame[4], uint16_t holdMs = 1000) {
+void showCommandFrame(const uint32_t frame[4], uint16_t holdMs = 1200) {
   matrix.loadFrame(frame);
   delay(holdMs);
-}
-
-void blinkFrame(const uint32_t frame[4], uint8_t times, uint16_t onMs, uint16_t offMs) {
-  for (uint8_t i = 0; i < times; i++) {
-    matrix.loadFrame(frame);
-    delay(onMs);
-    matrix.clear();
-    delay(offMs);
-  }
-  matrix.loadFrame(IdleFrame);
+  matrix.loadFrame(HeartFrame);
 }
 
 void setup() {
   matrix.begin();
   matrix.clear();
-  matrix.loadFrame(IdleFrame);
+  matrix.loadFrame(HeartFrame);
 
   Bridge.begin();
   Bridge.provide("warmer_light", warmer_light);
@@ -72,18 +68,17 @@ void setup() {
 void loop() {}
 
 void warmer_light() {
-  blinkFrame(WarmFrame, 2, 350, 120);
+  showCommandFrame(SunFrame);
 }
 
 void cooler_light() {
-  blinkFrame(CoolFrame, 2, 350, 120);
+  showCommandFrame(SnowflakeFrame);
 }
 
 void dimmer() {
-  showFrame(DimFrame, 900);
-  matrix.loadFrame(IdleFrame);
+  showCommandFrame(MinusFrame);
 }
 
 void brighter() {
-  blinkFrame(BrightFrame, 2, 180, 120);
+  showCommandFrame(PlusFrame);
 }
